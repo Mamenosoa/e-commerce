70df8f4b79ca8fcbde059c2e89edcd198e04bc4d
Mamenosoa <nomenjanaharymamenosoa@gmail.com>
2024-06-19 11:09:29 +0300

    reset height ajustement

--- app/landing/HeroSection.tsx
+++ app/landing/HeroSection.tsx
@@ -3,11 +3,11 @@ import { Navbar } from "../ui/Navbar";
 import image from '@/public/sneakers.png'
 export function Hero(){
     return(<>                     
-        <div className="flex w-full h-screen pb-xl pt-sm relative max-h-[800px] min-h-[700px] ">
+        <div className="flex w-full h-screen pb-xl pt-sm relative max-h-[800px]  ">
            {/* <div className="relative "> */}
                 <div className=" flex flex-col  justify-between  ">
                                 <Navbar/>
-                            <div className="min-md:max-w-[510px] max-lg:w-[780px]">
+                            <div className="min-md:max-w-[510px] max-lg:w-[780px] relative">
                                 <p className="text-sm mb-sm max-md:text-sm">A NEW, APPROACHABLE FACE FOR YOUR ENVIRONMENTALE CONSCIOUS SHOPPING</p>
                                 <p className="text-6xl mb-lg max-md:text-4xl max-lg:w-full ">Enjoy limeted series sneakers without limitations</p>
                                 <button className="mb-lg px-lg py-4 border-2 rounded-full max-md:text-sm  border-[#FFAF00]">SHOP NOW</button>
