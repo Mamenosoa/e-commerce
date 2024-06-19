41ef12c32bbd4ed7f5b7fdcfdb51c1fa2e1569a8
Mamenosoa <nomenjanaharymamenosoa@gmail.com>
2024-06-19 12:15:53 +0300

    test

--- app/landing/HeroSection.tsx
+++ app/landing/HeroSection.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "../ui/Navbar";
 import image from '@/public/sneakers.png'
 export function Hero(){
     return(<>                     
-        <div className="flex w-full h-screen pb-xl max-md:h-[620px]  pt-sm relative max-h-[768px]  ">
+        <div className="flex w-full h-screen pb-xl max-md:h-[620px]  pt-sm relative max-h-[768px] ">
            {/* <div className="relative "> */}
-                <div className=" flex flex-col   justify-between  ">
+                <div className=" flex flex-col min-h-[612px]  justify-between  ">
                                 <Navbar/>
                             <div className="min-md:max-w-[510px] max-lg:max-w-[780px] relative">
                                 <p className="text-sm text-bold mb-sm max-md:text-sm">A NEW, APPROACHABLE FACE FOR YOUR ENVIRONMENTALE CONSCIOUS SHOPPING</p>
@@ -18,15 +18,17 @@ export function Hero(){
                         </div>
                     </div>
            {/* </div> */}
-            <div className="relative flex justify-end items-center w-full h-full  max-lg:hidden  ">
-                <div className=" w-100  max-md:hidden relative h-full flex items-center flex-col justify-end pl-xl pb-xl bg-[#E2E5FF] ">
-                <div className="w-80 h-80 overflow-hidden">
-                    <Image src={image} alt="sneakers" width={400} height={400} className="block" />
-                </div>
-                <div className="w-full pr-xl">
-                    <p className="text-xl text-bold mb-sm">TRAVIS SCOTT X AIR JORDAN 4 RETRO CATCUS JACK</p>
-                    <div className="text-xs rounded-full text-white bg-black w-20 py-1 text-center ">AirJordan</div>
-                </div>
+            <div className="relative flex justify-end items-center w-full h-full min-h-[612px]  max-lg:hidden  ">
+                <div className=" w-100  max-md:hidden relative h-full flex items-center flex-col justify-center pl-xl pb-xl bg-[#E2E5FF] ">
+                    <div>
+                    <div className="w-80 h-80 overflow-hidden">
+                        <Image src={image} alt="sneakers" width={400} height={400} className="block" />
+                    </div>
+                    <div className="w-full pr-xl">
+                        <p className="text-xl text-bold mb-sm">TRAVIS SCOTT X AIR JORDAN 4 RETRO CATCUS JACK</p>
+                        <div className="text-xs rounded-full text-white bg-black w-20 py-1 text-center ">AirJordan</div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
